b2b578621435fa784dafe2b47c3a15039c30151d
DruidicBearClan <l.lindquist@live.nl>
2023-05-03 00:58:35 +0200

    More packages and adjustments, built a pause and echoes

--- unattended-install-ubuntu2204.sh
+++ unattended-install-ubuntu2204.sh
@@ -1,33 +1,37 @@
 #~/bin/bash
+# init
+function pause(){
+   read -p "$*"
+}
 
-#Regular ubuntu mirrorlist update and upgrade all apps
+pause 'This script assumes that you have a new ubuntu install or that you at least have read some of what the script does. Press any key to confirm that, or cancel with Ctrl-C'
+
+echo updating regular packages
 apt get update && apt get upgrade -y
 
-#Installing the essential net-tools and media
+echo Installing the essential net-tools and media
 apt install net-tools openssh whois ldnsutils ffmpeg traceroute nmap -y
 
-#installing required libraries and older themes for non-gtk3 apps
+echo installing required libraries and older themes for non-gtk3 apps
 apt install qt5-style-kvantum qt5-style-kvantum-themes libssl-dev libssh-dev libidn11-dev libpcre3-dev -y
 
-#Installing essential applications
+echo installing some essential software
 apt install baobab rlwrap ufw htop vim git vlc eog evolution-ews gnome-shell-extension-manager chrome-gnome-shell gnome-tweaks tilix -y
 
-#Install security tools included in default ubuntu mirrors (where held version is no concern)
-apt install proxychains hashcat gpg pgpgpg tor proxychains default-mysql-client libimage-exiftool-perl
+echo installing Ubuntu-repo included security tools for which the release version is not an issue
+apt install proxychains hashcat gpg pgpgpg tor proxychains default-mysql-client libimage-exiftool-perl -y
 
-#Enable a simple local firewall with a default deny policy
+echo enabling UFW firewall with default deny rules
 ufw enable
 ufw default deny incoming
 ufw default allow outgoing
 ufw reload
-ufw status numbered
 
-#Create some extra folders in home directory
+echo creating multiple folders in home directory
 mkdir /home/$USER/.icons /home/$USER/.themes /home/$USER/Apps /home/$USER/Tools /home/$USER/Dev
 
-echo select the number for option tilix terminal as default and confirm the choice
+echo Now select tilix terminal from the options and confirm the choice with ENTER
 
-#Sets tilix application as the default terminal, adds tilix as context-menu option, remove ubuntu terminal as option
 update-alternatives --config x-terminal-emulator
 apt install python3-nautilus
 apt remove nautilus-extension-gnome-terminal
