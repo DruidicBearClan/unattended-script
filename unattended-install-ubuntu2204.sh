b4acbdd365e13b22a0ccb118cc0b9d4b48b07838
DruidicBearClan <l.lindquist@live.nl>
2023-05-03 01:13:19 +0200

    more changes

--- unattended-install-ubuntu2204.sh
+++ unattended-install-ubuntu2204.sh
@@ -1,12 +1,18 @@
 #~/bin/bash
-# init
+
+#functions
+
 function pause(){
    read -p "$*"
 }
 
-pause 'This script assumes that you have a new ubuntu install or that you at least have read some of what the script does. 
-Press any key to confirm that, or cancel with Ctrl-C'
+#init
 
+#motd
+pause 'This script assumes that you have a new ubuntu install or that you at least have read some of what the script does. 
+Press any key to confirm that, or cancel with Ctrl-C. I recommend the Ubuntu desktop minimal install for this script.'
+
+#updating and installing
 echo updating regular packages
 apt get update && apt get upgrade -y
 
@@ -22,15 +28,18 @@ apt install baobab rlwrap ufw htop vim git vlc eog evolution-ews gnome-shell-ext
 echo installing Ubuntu-repos included security tools, for which the release version is not an issue
 apt install proxychains hashcat gpg pgpgpg tor default-mysql-client libimage-exiftool-perl -y
 
+#installing uncomplicated fire wall
 echo enabling UFW firewall with default deny rules
 ufw enable
 ufw default deny incoming
 ufw default allow outgoing
 ufw reload
 
+#creating some often used folders
 echo creating multiple folders in home directory
 mkdir /home/$USER/.icons /home/$USER/.themes /home/$USER/Apps /home/$USER/Tools /home/$USER/Dev
 
+#configuring tilix default terminal application and fixing context menu
 echo Now select tilix terminal from the options and confirm the choice with ENTER
 
 update-alternatives --config x-terminal-emulator
