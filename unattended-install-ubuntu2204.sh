7334ce35820ef0d17ef1f52dbd0a4ba796d634a5
DruidicBearClan <l.lindquist@live.nl>
2023-06-30 22:53:13 +0200

    more additions

--- unattended-install-ubuntu2204.sh
+++ unattended-install-ubuntu2204.sh
@@ -9,8 +9,8 @@ function pause(){
 #init
 
 #MOTD
-pause 'This script assumes that you have a new Ubuntu install. This script will do a lot of stuff. Read the code.
-Press any key to confirm, or cancel with Ctrl-C. tested on Ubuntu Desktop 22.04 minimal version'
+pause 'This script assumes that you have a new Ubuntu install. This script will do a lot of stuff, read the code.
+Press any key to confirm, or cancel with Ctrl-C. Tested on Ubuntu Desktop 22.04 minimal version'
 
 #updating and installing
 sleep 5 updating regular packages
@@ -26,7 +26,9 @@ echo installing some essential software
 apt install baobab rlwrap ufw htop vim git vlc eog evolution-ews gnome-shell-extension-manager chrome-gnome-shell gnome-tweaks tilix -y
 
 echo installing the packages for which the release version is not an issue, grabbing those from default Ubuntu repo.
-apt install proxychains wireshark meld hashcat gpg pgpgpg sqlitebrowser tor default-mysql-client libimage-exiftool-perl -y
+apt install proxychains slapd ldap-utils wireshark meld hashcat gpg pgpgpg sqlitebrowser tor default-mysql-client libimage-exiftool-perl -y
+
+systemctl enable slapd
 
 #installing uncomplicated fire wall
 echo enabling UFW firewall with default deny rules
@@ -47,3 +49,21 @@ apt install python3-nautilus
 apt remove nautilus-extension-gnome-terminal
 killall nautilus
 mv /usr/share/applications/org.gnome.Terminal.desktop /usr/share/applications/org.gnome.Terminal.desktop_bak
+
+#Disable Wayland, enable X11, blackist nouveau
+sed 's/#WaylandEnable=false/WaylandEnable=false/g' /etc/gdm3/custom.conf
+
+bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+
+#Update the kernel
+update-initramfs -u
+
+#Creating a default 4GB swap partition
+swapoff -a
+dd if=/dev/zero of=/swapfile bs=1M count=4096
+mkswap /swapfile
+swapon /swapfile
+free -m
+
+echo A reboot of the system is now required.
